49b09043bc06909ee57be57054be29bd70fd22ec
Amauri Lopez <lopeza16@stem112-mac14.local>
2017-11-30 16:50:28 -0500

    Updated Routing and other things

--- code/Gud-Food/MapSettings.swift
+++ code/Gud-Food/MapSettings.swift
@@ -10,10 +10,20 @@ import Foundation
 
 class MapSettings: UITableViewController{
     
+    @IBOutlet weak var UndoSettingChangesButton: UIBarButtonItem!
+    @IBOutlet weak var SaveSettingsButton: UIBarButtonItem!
+    @IBOutlet weak var WalkingSwitch: UISwitch!
+    @IBOutlet weak var BusSwitch: UISwitch!
+    @IBOutlet weak var DrivingSwitch: UISwitch!
+    @IBOutlet weak var StampsSwitch: UISwitch!
     @IBOutlet weak var OpenNavTab: UIBarButtonItem!
     
-    var SettingsTableArray = [String]() //array of strings
+    private var SettingsTableArray = [Bool]() //array of Boolean values for each setting
     
+    private var stampsVal = Bool()
+    private var drivingVal = Bool()
+    private var busVal = Bool()
+    private var walkingVal = Bool()
     
     override func viewDidLoad() {
         
@@ -22,6 +32,62 @@ class MapSettings: UITableViewController{
         
         
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer()) //add the gesture recognition
+        
+        //grab initial states of switches
+        SettingsTableArray[0] = StampsSwitch.isOn
+        SettingsTableArray[1] = DrivingSwitch.isOn
+        SettingsTableArray[2] = DrivingSwitch.isOn
+        SettingsTableArray[3] = DrivingSwitch.isOn
+        
+    }
+    
+    //gettings and setters
+    func getSettings() -> Array<Bool>{
+        return SettingsTableArray
+    }
+    func setSettings(stampsVal: Bool, drivingVal: Bool, busVal: Bool, walkingVal: Bool){
+        SettingsTableArray[0] = stampsVal
+        SettingsTableArray[1] = drivingVal
+        SettingsTableArray[2] = busVal
+        SettingsTableArray[3] = walkingVal
+    }
+    
+    @IBAction func stampsValChanged(_ sender: UISwitch) {
+        SaveSettingsButton.isEnabled = true
+        UndoSettingChangesButton.isEnabled = true
+        stampsVal = sender.isOn
+    }
+    
+    @IBAction func drivingValChanged(_ sender: UISwitch) {
+        SaveSettingsButton.isEnabled = true
+        UndoSettingChangesButton.isEnabled = true
+        drivingVal = sender.isOn
+    }
+    
+    @IBAction func busValChanged(_ sender: UISwitch) {
+        SaveSettingsButton.isEnabled = true
+        UndoSettingChangesButton.isEnabled = true
+        busVal = sender.isOn
+    }
+    
+    @IBAction func walkingValChanged(_ sender: UISwitch) {
+        SaveSettingsButton.isEnabled = true
+        UndoSettingChangesButton.isEnabled = true
+        walkingVal = sender.isOn
+    }
+    
+    
+    //save the setting and repopulate the settings table
+    @IBAction func saveSettings(_ sender: UIBarButtonItem) {
+        setSettings(stampsVal: stampsVal, drivingVal: drivingVal, busVal: busVal, walkingVal: walkingVal)
+    }
+    
+    //undo current changes, revert the buttons back to original states
+    @IBAction func undoChanges(_ sender: UIBarButtonItem) {
+        StampsSwitch.isOn = SettingsTableArray[0]
+        DrivingSwitch.isOn = SettingsTableArray[1]
+        BusSwitch.isOn = SettingsTableArray[2]
+        WalkingSwitch.isOn = SettingsTableArray[3]
     }
     
     
--- code/Gud-Food/Routing.swift
+++ code/Gud-Food/Routing.swift
@@ -12,7 +12,55 @@ import UIKit
 import CoreLocation
 
 class Routing {
-    func Route(destination: MKPointAnnotation){
+    
+    var settings = MapSettings()
+    func Route(destination: MKPointAnnotation) -> MKRoute{
         
+        //create placemarks and map items for source and destination
+        let sourcePlacemark = MKPlacemark(coordinate: destination.coordinate)
+        let destPlacemark = MKPlacemark(coordinate: destination.coordinate)
+        
+        let sourceItem = MKMapItem(placemark: sourcePlacemark)
+        let destItem = MKMapItem(placemark: destPlacemark)
+        
+        //populate a directions request array
+        var directionRequests = Array<MKDirectionsRequest>()
+        
+        var settingsTable = settings.getSettings() //get current settings to filter routes
+        if settingsTable[1] == true{
+            //directions request from Apple Cloud
+            directionRequests[0].source = sourceItem
+            directionRequests[0].destination = destItem
+            directionRequests[0].transportType = .automobile
+        }//if driving directions is enabled
+        
+        if settingsTable[2] == true{
+            directionRequests[1].source = sourceItem
+            directionRequests[1].destination = destItem
+            directionRequests[1].transportType = .transit
+        }
+        if settingsTable[3] == true{
+            directionRequests[2].source = sourceItem
+            directionRequests[2].destination = destItem
+            directionRequests[2].transportType = .walking
+        }
+        
+        
+        let directions = MKDirections(request: directionRequest)
+        var route = MKRoute()
+        directions.calculate(completionHandler: {
+            response, error in
+            
+            guard let response = response else{
+                if let error = error{
+                    print("Something went wrong, there might not be a driving route")
+                }
+                return
+            }
+            
+            route = response.routes[0] //grab first route (fastest one)
+
+        })
+        return route
     }
 }
